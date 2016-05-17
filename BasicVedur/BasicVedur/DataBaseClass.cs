d7f309839598135ecd55788f1f1beb2e04ddbefc
kollibjarki <kolbeinnsvavarsson@gmail.com>
2016-05-17 15:36:43 +0000

    Add files via upload

---
+++ BasicVedur/BasicVedur/DataBaseClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicVedur
+{
+    class DataBaseClass
+    {
+
+        private void Update()
+        {
+            //LandshlutarTableAdapter lta = new LandshlutarTableAdapter();
+            //StadirTableAdapter sta = new StadirTableAdapter();
+            //LandshlutarL.ItemsSource = lta.GetData();
+            //stadirC.ItemsSource = sta.GetData();
+
+
+        }
+        
+    }
+}
